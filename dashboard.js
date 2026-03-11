e67a7a3b350c291f2b64957dee0327709b6dc419
itsfrosea <frostyyuniverse@gmail.com>
2026-03-11 16:50:39 +0530

    Updated

--- dashboard.js
+++ dashboard.js
@@ -1,3 +1,14 @@
+function toggleSidebar(){
+
+const sidebar = document.getElementById("sidebar")
+
+if(sidebar.style.display === "none"){
+sidebar.style.display = "block"
+}else{
+sidebar.style.display = "none"
+}
+
+}
 
 
 function openPage(id){
