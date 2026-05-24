f0bae3c17f4f6adbfee3bac076bc92442e5bc8fb
itsfrosea <frostyyuniverse@gmail.com>
2026-05-24 09:23:23 +0530

    another

--- dashboard.js
+++ dashboard.js
@@ -570,6 +570,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    lucide.createIcons();
+
+    const sidebar = document.getElementById("sidebar");
+    const toggle = document.getElementById("toggleSidebar");
+
+    toggle.addEventListener("click", () => {
+        sidebar.classList.toggle("collapsed");
+    });
+
+});
+
 // =====================
 // START
 // =====================
